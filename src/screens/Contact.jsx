bebd5909a012a3ba28cbd9b4f04bf4ddc7183fca
Jodi Alcantara <jjalcantara1@student.hau.edu.ph>
2024-09-07 14:46:48 +0800

    contact images

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -3,9 +3,9 @@ import emailjs from 'emailjs-com';
 import Header from '../components/Header';
 import { FaInstagram, FaFacebook, FaLink, FaPhone, FaEnvelope, FaMapMarkerAlt } from 'react-icons/fa';
 
-import './Contact.css';
-
-import section3bg from '../img/section3bg.png'
+import './Contact.css'; // Import the external stylesheet
+import contact1 from '../img/contact1.jpg'; // Import image
+import contact2 from '../img/contact2.jpg'; // Import image
 
 const Contact = () => {
   const [formData, setFormData] = useState({
@@ -46,123 +46,54 @@ const Contact = () => {
   return (
     <>
       <Header />
-      <div className='contactdiv' style={{
-        // backgroundImage:`url(${section3bg})`,
-        // backgroundRepeat: 'no-repeat',
-        // backgroundSize: 'cover',
-        backgroundColor: '#0a5c36',
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        padding: '20px',
-        color: 'white',
-      }}>
-        <div style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'flex-start',
-          width: '80%',
-          flexWrap: 'wrap',
-        }}>
-          <aside style={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '40px' }}>
-            <div style={{
-              position: 'relative',
-              width: 'fit-content',
-              borderRadius: '50%',
-              boxShadow: '-4px -4px 0px 5px #FFBD59',
-              zIndex: 1,
-              marginLeft: '-150px',
-            }}>
-              <img
-                src="https://cdn.discordapp.com/attachments/1216019980852658216/1280760643888222250/download.webp?ex=66d940cc&is=66d7ef4c&hm=51c1f09b06ad94a44081f32f386dfc6c3a27157a72333a31cfcd0e7d5f39d994&"
-                style={{ borderRadius: '50%', maxWidth: '30rem', margin: 'auto' }}
-              />
+      <div className='contact-container'>
+        <div className='contact-content'>
+          <aside className='contact-aside'>
+            <div className='contact-image-wrapper'>
+              <img src={contact1} className='contact-image-large' alt="Contact 1" />
             </div>
-            <div style={{
-              position: 'relative',
-              width: 'fit-content',
-              borderRadius: '50%',
-              boxShadow: '-4px -4px 0px 5px #FFBD59',
-              zIndex: 2,
-              marginTop: '-100px',
-              marginLeft: '120px',
-            }}>
-              <img
-                src="https://cdn.discordapp.com/attachments/1216019980852658216/1280760643888222250/download.webp?ex=66d940cc&is=66d7ef4c&hm=51c1f09b06ad94a44081f32f386dfc6c3a27157a72333a31cfcd0e7d5f39d994&"
-                style={{ borderRadius: '50%', maxWidth: '20rem', margin: 'auto' }}
-              />
+            <div className='contact-image-wrapper contact-image-wrapper-small'>
+              <img src={contact2} className='contact-image-small' alt="Contact 2" />
             </div>
           </aside>
 
-          <section style={{ maxWidth: '500px', textAlign: 'left' }}>
-            <p style={{
-              fontSize: '4rem',
-              fontWeight: 'bold',
-              textShadow: '4px 4px #04371F, 6px 6px #ff5757',
-              marginBottom: '30px',
-              color: '#FFBD59',
-              textDecoration: 'underline',
-              alignItems: 'center',
-              justifyContent: 'center',
-              textAlign: 'center',
-              fontFamily: '"Lobster", cursive',
-              fontWeight: '700',
-              fontStyle: 'normal',
-            }}>GET IN TOUCH!</p>
-            <div style={{ marginBottom: '20px', paddingLeft: '2rem' }}>
-              <div style={{ display: 'flex', alignItems: 'center', margin: '10px 0', fontSize: '1.5rem' }}>
-                <FaPhone style={{ marginRight: '15px', color: '#FFBD59'  }} />
-                <span style={{ color: 'white', fontFamily: '"Itim", sans-serif', fontWeight: '400', fontStyle: 'normal', color: '#FFBD59', textShadow: '2px 2px #04371F'  }}>0966 163 7748</span>
+          <section className='contact-info'>
+            <p className='contact-title'>GET IN TOUCH!</p>
+            <div className='contact-details'>
+              <div className='contact-detail'>
+                <FaPhone className='contact-icon' />
+                <span>0966 163 7748</span>
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', margin: '10px 0', fontSize: '1.5em' }}>
-                <FaEnvelope style={{ marginRight: '15px', color: '#FFBD59' }} />
-                <span style={{ color: 'white', fontFamily: '"Itim", sans-serif', fontWeight: '400', fontStyle: 'normal', color: '#FFBD59', textShadow: '2px 2px #04371F' }}>gabs.chickeninasal@gmail.com</span>
+              <div className='contact-detail'>
+                <FaEnvelope className='contact-icon' />
+                <span>gabs.chickeninasal@gmail.com</span>
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', margin: '10px 0', fontSize: '1.5em' }}>
-                <FaInstagram style={{ marginRight: '15px', color: '#FFBD59' }} />
-                <a href="https://www.instagram.com/gabs.chickeninasal/" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none', fontFamily: '"Itim", sans-serif', fontWeight: '400', fontStyle: 'normal', color: '#FFBD59', textShadow: '2px 2px #04371F' }}>Instagram</a>
+              <div className='contact-detail'>
+                <FaInstagram className='contact-icon' />
+                <a href="https://www.instagram.com/gabs.chickeninasal/" target="_blank" rel="noopener noreferrer">Instagram</a>
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', margin: '10px 0', fontSize: '1.5em' }}>
-                <FaFacebook style={{ marginRight: '15px', color: '#FFBD59' }} />
-                <a href="https://www.facebook.com/Gabs.ChickenInasal/" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none', fontFamily: '"Itim", sans-serif', fontWeight: '400', fontStyle: 'normal', color: '#FFBD59', textShadow: '2px 2px #04371F' }}>Facebook</a>
+              <div className='contact-detail'>
+                <FaFacebook className='contact-icon' />
+                <a href="https://www.facebook.com/Gabs.ChickenInasal/" target="_blank" rel="noopener noreferrer">Facebook</a>
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', margin: '10px 0', fontSize: '1.5em' }}>
-                <FaLink style={{ marginRight: '15px', color: '#FFBD59' }} />
-                <a href="https://linktr.ee/gabs.chickeninasal?fbclid=IwY2xjawE8BEtleHRuA2FlbQIxMAABHUzN0IjfBz1eKl6B1-SfFIanv8iZV17huGmOWigOHx5JZPm86IU0QylSBQ_aem_K8LR6d5MFF0-VYj8ra50Nw" target="_blank" rel="noopener noreferrer" style={{ color: 'white', textDecoration: 'none', fontFamily: '"Itim", sans-serif', fontWeight: '400', fontStyle: 'normal', color: '#FFBD59', textShadow: '2px 2px #04371F' }}>Linktree</a>
+              <div className='contact-detail'>
+                <FaLink className='contact-icon' />
+                <a href="https://linktr.ee/gabs.chickeninasal?fbclid=IwY2xjawE8BEtleHRuA2FlbQIxMAABHUzN0IjfBz1eKl6B1-SfFIanv8iZV17huGmOWigOHx5JZPm86IU0QylSBQ_aem_K8LR6d5MFF0-VYj8ra50Nw" target="_blank" rel="noopener noreferrer">Linktree</a>
               </div>
-              <div style={{ display: 'flex', alignItems: 'center', margin: '10px 0', fontSize: '1.5em' }}>
-                <FaMapMarkerAlt style={{ marginRight: '15px', color: '#FFBD59' }} />
-                <span style={{ color: 'white', fontFamily: '"Itim", sans-serif', fontWeight: '400', fontStyle: 'normal', color: '#FFBD59', textShadow: '2px 2px #04371F' }}>The Shoppes at Infinity, Brgy. Pulung Maragul, Angeles City, Philippines</span>
+              <div className='contact-detail'>
+                <FaMapMarkerAlt className='contact-icon' />
+                <span>The Shoppes at Infinity, Brgy. Pulung Maragul, Angeles City, Philippines</span>
               </div>
             </div>
           </section>
         </div>
 
-        <div style={{
-          marginTop: '3rem',
-          borderBottom: '0.23rem dotted #FFBD59',
-          width: '85%'
-        }}></div>
+        <div className='contact-divider'></div>
 
-        <div><h1 style={{
-              fontSize: '4rem',
-              fontWeight: 'bold',
-              textShadow: '4px 4px #04371F, 6px 6px #ff5757',
-              // marginBottom: '30px',
-              marginTop: '3rem',
-              color: '#FFBD59',
-              alignItems: 'center',
-              justifyContent: 'center',
-              textAlign: 'center',
-              fontFamily: '"Lobster", cursive',
-              fontWeight: '700',
-              fontStyle: 'normal',
-            }}>Send us a Message!</h1></div>
+        <h1 className='contact-form-title'>Send us a Message!</h1>
 
-        {/* Contact Form and Google Maps Section */}
-        <div style={{
+         {/* Contact Form and Google Maps Section */}
+         <div style={{
           display: 'flex',
           width: '80%',
           justifyContent: 'space-between',
